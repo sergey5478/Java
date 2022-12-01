4243163ff657d2a8c76abfd20f5f1c3f300ffa83
sergey <sergey.fomin.90@inbox.ru>
2022-12-01 21:48:52 +0400

    chnges_sem3

--- Seminar3/Home3.java
+++ Seminar3/Home3.java
@@ -16,13 +16,11 @@ public class Home3 {
         if (startIndex >= endIndex - 1) {
             return buffer1;
         }
-
-        //уже отсортирован
+        
         int middle = startIndex + (endIndex - startIndex) / 2;
         int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
         int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
-
-        //слияние
+        
         int index1 = startIndex;
         int index2 = middle;
         int destIndex = startIndex;
