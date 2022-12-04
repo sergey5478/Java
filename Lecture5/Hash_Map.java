d862b2fb36256fafe84e1c1ec9fd64701bcb2b6d
sergey <sergey.fomin.90@inbox.ru>
2022-12-04 14:42:53 +0400

    add_lec5

---
+++ Lecture5/Hash_Map.java
@@ -0,0 +1,21 @@
+package Lecture5;
+
+import java.util.*;
+
+public class Hash_Map {
+    public static void main(String[] args) {
+        Map<Integer, String> db = new HashMap<>();
+        db.putIfAbsent(1, "один"); 
+        db.put(2, "два"); 
+        db.put(null, "!null"); 
+        System.out.println(db); // {null=!null, 1=один, 2=два}
+        System.out.println(db.get(2));//db.remove(null);        
+        System.out.println(db); // {1=один, 2=два}
+        System.out.println(db.containsValue("один")); // true
+        System.out.println(db.containsValue(1)); // false
+        System.out.println(db.containsKey("один")); // false
+        System.out.println(db.containsKey(1)); // true
+        System.out.println(db.keySet());
+        System.out.println(db.values());
+    }
+}
